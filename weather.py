848dce6e1805b80c5ff460d84cec72e6dbd2fb06
GKacyW <gkwark04@gmail.com>
2024-04-06 08:31:18 -0400

    more stuff :D

--- weather.py
+++ weather.py
@@ -1,9 +1,13 @@
 import requests
+from suntime import Sun
+from datetime import datetime
+import pytz 
+from tzlocal import get_localzone
 
 api_key = 'a04ce32f6a47777c9dd312e6da67966b'
 #default settings
 
-def weather(city, unit):
+def get_weather(city, unit):
 
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 
@@ -25,5 +29,32 @@ def weather(city, unit):
             tempre = "uh oh" 
     else:
         print("uh oh")
+    
+    return_list = [tempre, desc]
+    return return_list
 
-    return(tempre, desc)
+def get_time(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        lon = data['coord']['lon']
+        lat = data['coord']['lat']
+        
+        sun = Sun(lat, lon)
+        date = datetime.today().date()
+        sun_rise = sun.get_local_sunrise_time(date)
+        sun_set = sun.get_local_sunset_time(date)
+        print(sun_set)
+
+        utc_now = datetime.utcnow()
+        local_tz = get_localzone()
+        local_datetime = local_tz.localize(sun_rise)
+        print(local_datetime)
+    else:
+        print("uh oh")
+
+if __name__ == "__main__":
+    test = get_time("clearwater")
--- weather_app.py
+++ weather_app.py
@@ -1,22 +1,36 @@
 import pygame
-import weather
-from astral.sun import sun
+from weather import *
+import datetime
 
 pygame.init()
 screen = pygame.display.set_mode((400,450))
 clock = pygame.time.Clock()
 running = True
 
-weather_icons = {
-    "clear": {"day": "images/clear.png", "night": "images/clear night.png"},
-    "cloudy": {"day": "images/cloudy.png", "night": "images/cloudy night.png"},
-    "rain": {"day": "images/rain.png", "night": "images/rain night.png"}
-}
 
+# weather specific code
+
+weather_icons = {
+    "clear sky": {"day": "images/clear.png", "night": "images/clear night.png"},
+    "few clouds": {"day": "images/scatted clouds.png", "night": "images/scatted clouds night.png"},
+    "scatted clouds": {"day": "images/scatted clouds.png", "night": "images/scatted clouds night.png"},
+    "broken clouds":{"day": "images/cloudy.png", "night": "images/cloudy night.png"},
+    "shower rain": {"day": "images/rain shower.png", "night": "images/rain shower night.png"},
+    "rain": {"day": "images/rain.png", "night": "images/rain night.png"},
+    "thunderstorm": {"day": "images/thunderstorm.png", "night": "images/thunderstorm night.png"},
+    "snow": {"day": "images/snow.png", "night": "images/snow night.png"},
+    "mist": {"day": "images/mist.png", "night": "images/mist night.png"}
+}
 bar = {
     "day": "images/day_bar.png",
     "night": "images/night_bar.png"
 }
+
+city = "Gainesville, Florida, USA"
+unit = "F"
+
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,15 +38,24 @@ while running:
     
     screen.fill((184,215,233))
     #do weather determinations
-    weather_condition = "rain"
-    time = "day"
-    image_path = weather_icons.get(weather_condition, {}).get(time)
+    weather_data = get_weather(city, unit)
+    weather_condition = weather_data[1]
+
+    #time determinations
+    now = datetime.datetime.now()
+    hour = now.hour
+    minute = now.minute
+
+    time = "night"
+
+    image_path = (weather_icons.get(weather_condition, {}).get(time))
     image = pygame.image.load(image_path).convert_alpha()
     bar_path = bar.get(time)
     bar_ = pygame.image.load(bar_path).convert_alpha()
 
     screen.blit(bar_,(25,10))
     screen.blit(image, (30,20))
+    
     
     pygame.display.flip()
     
