b92991e30902332cdcebe7d9bf1b2a6882166c4c
GKacyW <gkwark04@gmail.com>
2024-04-05 23:26:54 -0400

    more updates!
    added images

--- weather_app.py
+++ weather_app.py
@@ -1,17 +1,38 @@
 import pygame
 import weather
+from astral.sun import sun
 
 pygame.init()
 screen = pygame.display.set_mode((400,450))
 clock = pygame.time.Clock()
 running = True
 
+weather_icons = {
+    "clear": {"day": "images/clear.png", "night": "images/clear night.png"},
+    "cloudy": {"day": "images/cloudy.png", "night": "images/cloudy night.png"},
+    "rain": {"day": "images/rain.png", "night": "images/rain night.png"}
+}
+
+bar = {
+    "day": "images/day_bar.png",
+    "night": "images/night_bar.png"
+}
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
     screen.fill((184,215,233))
-    pygame.draw.rect(screen, "Red", [30,20,400-50,90], 4)
+    #do weather determinations
+    weather_condition = "rain"
+    time = "day"
+    image_path = weather_icons.get(weather_condition, {}).get(time)
+    image = pygame.image.load(image_path).convert_alpha()
+    bar_path = bar.get(time)
+    bar_ = pygame.image.load(bar_path).convert_alpha()
+
+    screen.blit(bar_,(25,10))
+    screen.blit(image, (30,20))
+    
     pygame.display.flip()
     
