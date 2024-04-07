41c4686cf834c04eabb28dae9f05d8dc76274ca7
GKacyW <gkwark04@gmail.com>
2024-04-06 23:20:03 -0400

    FINISHED PRODUCT
    task tabby :)

---
+++ GUI.py
@@ -0,0 +1,200 @@
+import pygame
+import os
+from buttons import *
+
+pygame.init()
+
+# Initialize Screen
+WIDTH, HEIGHT = 400, 450
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("To-Do App.")
+
+# Colors
+BABY_BLUE = (184, 215, 233)
+LIST_BLUE = (135, 206, 250)
+SLIGHTLY_DARKER_BLUE = (91, 120, 201)
+WHITE = (255, 255, 255)
+
+# Initialize To-Do 
+dialog_box = pygame.Surface((250, 230))
+dialog_box.fill(LIST_BLUE)
+
+# Check Mark Image
+check_mark = pygame.image.load('checkmark.png')
+check_mark = pygame.transform.scale(check_mark, (20, 20))
+checkmark = pygame.image.load(os.path.join('checkmark.png'))
+
+# Running the Clock
+FPS = 60
+
+# Make Check Boxes
+y_coord_box = 30
+
+# Make Rectangles for Text
+box1 = box2 = box3 = box4 = box5 = pygame.image.load('text_box.png')
+box1 = box2 = box3 = box4 = box5 = pygame.transform.scale(check_mark, (100, 20))
+box1 = box2 = box3 = box4 = box5 = pygame.image.load(os.path.join('text_box.png'))
+
+# Generate Check Boxes
+def check_box(platform):
+    y_coord = 28
+    for i in range(5):
+        pygame.draw.rect(platform, WHITE, pygame.Rect(20, y_coord, 20, 20), 3)
+        y_coord += 40
+
+# Generate Box for Title
+def title_page():
+    title = pygame.Surface((240, 30))
+    title.fill(SLIGHTLY_DARKER_BLUE)
+    return title
+
+# Combine Everything onto the Screen
+def draw_window():
+
+    SCREEN.fill(BABY_BLUE)
+
+    # Blit the various screens
+    check_box(dialog_box)
+    todo_title = title_page()
+    SCREEN.blit(dialog_box, (15, 130))
+    SCREEN.blit(todo_title, (15, 115))
+    AddText(SCREEN, 26, "To-Do List", WHITE, 90, 120)
+
+    # Text Boxes
+    SCREEN.blit(box1, (60, 153))
+    SCREEN.blit(box2, (60, 193))
+    SCREEN.blit(box3, (60, 233))
+    SCREEN.blit(box4, (60, 273))
+    SCREEN.blit(box5, (60, 313))
+
+# Defining the buttons
+button1 = pygame.Rect(35, 158, 20, 20)
+button2 = pygame.Rect(35, 200, 20, 20)
+button3 = pygame.Rect(35, 237, 20, 20)
+button4 = pygame.Rect(35, 274, 20, 20)
+button5 = pygame.Rect(35, 311, 20, 20)
+
+# Main Loop
+def main():
+    image_click = False
+    user_input1, user_input2, user_input3, user_input4, user_input5 = "", "", "", "", ""
+    button1_state, button2_state, button3_state, button4_state, button5_state, = False, False, False, False, False
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+
+        # Start the windows
+        draw_window()
+        pygame.draw.rect(SCREEN, (255, 255, 255), button1, 3)
+       
+        # Typing Functionality for the Tasks
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box1.get_rect(center=(110, 163)).collidepoint(pygame.mouse.get_pos())): user_input1 = user_input1[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box1.get_rect(center=(110, 163)).collidepoint(pygame.mouse.get_pos())):
+                        user_input1 += event.unicode
+
+            AddText(SCREEN, 20, user_input1, (0,0,0), 70, 160)
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box2.get_rect(center=(125, 203)).collidepoint(pygame.mouse.get_pos())): user_input2 = user_input2[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box2.get_rect(center=(125, 203)).collidepoint(pygame.mouse.get_pos())):
+                        user_input2 += event.unicode
+
+            AddText(SCREEN, 20, user_input2, (0,0,0), 70, 200)
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box3.get_rect(center=(125, 243)).collidepoint(pygame.mouse.get_pos())): user_input3 = user_input3[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box3.get_rect(center=(125, 243)).collidepoint(pygame.mouse.get_pos())):
+                        user_input3 += event.unicode
+
+            AddText(SCREEN, 20, user_input3, (0,0,0), 70, 240)
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box4.get_rect(center=(125, 283)).collidepoint(pygame.mouse.get_pos())): user_input4 = user_input4[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box4.get_rect(center=(125, 283)).collidepoint(pygame.mouse.get_pos())):
+                        user_input4 += event.unicode
+
+            AddText(SCREEN, 20, user_input4, (0,0,0), 70, 280)
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box5.get_rect(center=(125, 323)).collidepoint(pygame.mouse.get_pos())): user_input5 = user_input5[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box5.get_rect(center=(125, 323)).collidepoint(pygame.mouse.get_pos())):
+                        user_input5 += event.unicode
+
+            AddText(SCREEN, 20, user_input5, (0,0,0), 70, 320)
+            
+            # Houses the Checkmark Functionality
+            pos = pygame.mouse.get_pos()
+            if button1.collidepoint(pos) or button2.collidepoint(pos) or button3.collidepoint(pos) or button4.collidepoint(pos) or button5.collidepoint(pos):
+                if button1.collidepoint(pos):
+                    SCREEN.blit(check_mark, (38, 155))
+                if button2.collidepoint(pos):
+                    SCREEN.blit(check_mark, (38, 195))
+                if button3.collidepoint(pos):
+                    SCREEN.blit(check_mark, (38, 235))
+                if button4.collidepoint(pos):
+                    SCREEN.blit(check_mark, (38, 275))
+                if button5.collidepoint(pos):
+                    SCREEN.blit(check_mark, (38, 315))
+
+            # Disappearing and Reappearing for Checkmarks.
+            # As well as selecting and deselecting.
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = pygame.mouse.get_pos()
+                print(click)
+                if button1.collidepoint(click) or button2.collidepoint(click) or button3.collidepoint(click) or button4.collidepoint(click) or button5.collidepoint(pos):
+                    image_click = True
+
+            if image_click:
+                image_click = False
+                if button1.collidepoint(click):
+                    SCREEN.blit(check_mark, (38, 155))
+                    print (button1_state)
+                    button1_state = not button1_state
+                if button2.collidepoint(click):
+                    SCREEN.blit(check_mark, (38, 195))
+                    button2_state = not button2_state
+                if button3.collidepoint(click):
+                    SCREEN.blit(check_mark, (38, 235))
+                    button3_state = not button3_state
+                if button4.collidepoint(click):
+                    SCREEN.blit(check_mark, (38, 275))
+                    button4_state = not button4_state
+                if button5.collidepoint(click):
+                    SCREEN.blit(check_mark, (38, 315))
+                    button5_state = not button5_state
+
+            if button1_state:
+                SCREEN.blit(check_mark, (38, 155))
+            if button2_state:
+                SCREEN.blit(check_mark, (38, 195))
+            if button3_state:
+                SCREEN.blit(check_mark, (38, 235))
+            if button4_state:
+                SCREEN.blit(check_mark, (38, 275))
+            if button5_state:
+                SCREEN.blit(check_mark, (38, 315))
+
+            # Update everything.
+            pygame.display.flip()
+
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
--- trying.py
+++ trying.py
@@ -9,6 +9,55 @@ import pygame.mixer
 from weather import *
 from datetime import timedelta, datetime
 from buttons import *
+import os
+
+# Initialize Screen
+WIDTH, HEIGHT = 400, 450
+pygame.display.set_caption("TaskTabby")
+
+# Colors
+BABY_BLUE = (184, 215, 233)
+LIST_BLUE = (135, 206, 250)
+SLIGHTLY_DARKER_BLUE = (91, 120, 201)
+WHITE = (255, 255, 255)
+
+# Initialize To-Do 
+dialog_box = pygame.Surface((240, 230))
+dialog_box.fill(LIST_BLUE)
+
+# Check Mark Image
+check_mark = pygame.image.load('checkmark.png')
+check_mark = pygame.transform.scale(check_mark, (20, 20))
+checkmark = pygame.image.load(os.path.join('checkmark.png'))
+background_4_timer = pygame.image.load("images/bg_4_timer.png")
+
+# Make Check Boxes
+y_coord_box = 30
+
+# Make Rectangles for Text
+box1 = box2 = box3 = box4 = box5 = pygame.image.load('text_box.png')
+box1 = box2 = box3 = box4 = box5 = pygame.transform.scale(check_mark, (100, 20))
+box1 = box2 = box3 = box4 = box5 = pygame.image.load(os.path.join('text_box.png'))
+
+# Generate Check Boxes
+def check_box(platform):
+    y_coord = 28
+    for i in range(5):
+        pygame.draw.rect(platform, WHITE, pygame.Rect(20, y_coord, 20, 20), 3)
+        y_coord += 40
+
+# Generate Box for Title
+def title_page():
+    title = pygame.Surface((240, 30))
+    title.fill(SLIGHTLY_DARKER_BLUE)
+    return title
+
+# Defining the buttons
+button1 = pygame.Rect(32, 158, 20, 20)
+button2 = pygame.Rect(32, 205, 20, 20)
+button3 = pygame.Rect(32, 242, 20, 20)
+button4 = pygame.Rect(32, 279, 20, 20)
+button5 = pygame.Rect(32, 316, 20, 20)
 
 #for pomodoro timer popup
 def withinTimer(seconds):
@@ -41,6 +90,7 @@ def main():
     selected_icon = pygame.transform.scale(selected, (15,15))
     empty = pygame.image.load('images/empty button.png')
     insert_city_bar = pygame.image.load('images/input city.png')
+    spotify_bar = pygame.image.load('images/background_spotify.png')
     empty_icon = pygame.transform.scale(empty, (15,15))
     city = "Gainesville, Florida, USA"
     unit = "C"
@@ -63,11 +113,11 @@ def main():
     font = pygame.font.Font('VT323-Regular.ttf', 20) #sets the font for all the text
 
     #displaying play/pause button
-    pauseButton = pygame.image.load('images/pause.png').convert()
+    pauseButton = pygame.image.load('images/pause.png').convert_alpha()
     pauseButton = pygame.transform.scale(pauseButton, (30, 30))
     buttonRect = pauseButton.get_rect()
     buttonRect.center = (330, 410)
-    playButton = pygame.image.load('images/play.png').convert()
+    playButton = pygame.image.load('images/play.png').convert_alpha()
     playButton = pygame.transform.scale(playButton, (30, 30))
 
     #try pausing Spotify immediately
@@ -106,27 +156,27 @@ def main():
         albumRect.center = (60, 410)
 
     #displaying next button
-    nextButton = pygame.image.load('images/arrow.png').convert()
+    nextButton = pygame.image.load('images/arrow.png').convert_alpha()
     nextRect = nextButton.get_rect()
     nextRect.center = (360, 410)
 
     #displaying previous button
-    prevButton = pygame.image.load('images/arrow.png').convert()
+    prevButton = pygame.image.load('images/arrow.png').convert_alpha()
     prevButton = pygame.transform.rotate(prevButton, 180)
     prevRect = prevButton.get_rect()
     prevRect.center = (300, 410)
 
     #little icon on line
-    lineIcon = pygame.image.load('images/icon.png').convert()
+    lineIcon = pygame.image.load('images/icon.png').convert_alpha()
     lineIcon = pygame.transform.scale(lineIcon, (10, 10))
     
     #timer setup
     chimeSound = pygame.mixer.Sound("chime.mp3")
-    TpauseButton = pygame.image.load('images/pause.png').convert()
+    TpauseButton = pygame.image.load('images/pause.png').convert_alpha()
     TpauseButton = pygame.transform.scale(TpauseButton, (30, 30))
     TbuttonRect = TpauseButton.get_rect()
-    TbuttonRect.center = (300, 250)
-    TplayButton = pygame.image.load('images/play.png').convert()
+    TbuttonRect.center = (300, 245)
+    TplayButton = pygame.image.load('images/play.png').convert_alpha()
     TplayButton = pygame.transform.scale(TplayButton, (30, 30))
     CLOCK = pygame.time.Clock()
     #default values for pomodoro timers
@@ -150,10 +200,14 @@ def main():
     timerLabelRect.center = (325, 165)
 
     #timer setting button
-    TsettingButton = pygame.image.load('images/timerIcon.png').convert()
+    TsettingButton = pygame.image.load('images/timerIcon.png').convert_alpha()
     TsettingButton = pygame.transform.scale(TsettingButton, (30, 30))
     TsettingRect = TsettingButton.get_rect()
-    TsettingRect.center = (350, 250)
+    TsettingRect.center = (350, 245)
+
+    image_click = False
+    user_input1, user_input2, user_input3, user_input4, user_input5 = "", "", "", "", ""
+    button1_state, button2_state, button3_state, button4_state, button5_state, = False, False, False, False, False
 
     #while loop for displaying everything
     while run_app:
@@ -161,12 +215,29 @@ def main():
         #coloring in screen
         screen.fill((184, 215, 233))
 
+        screen.fill(BABY_BLUE)
+
+        # Blit the various screens
+        check_box(dialog_box)
+        todo_title = title_page()
+        screen.blit(dialog_box, (15, 130))
+        screen.blit(todo_title, (15, 115))
+        AddText(screen, 26, "To-Do List", WHITE, 90, 120)
+
+        # Text Boxes
+        screen.blit(box1, (60, 153))
+        screen.blit(box2, (60, 193))
+        screen.blit(box3, (60, 233))
+        screen.blit(box4, (60, 273))
+        screen.blit(box5, (60, 313))
+
         #managing events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run_app = False
                 sp.pause_playback(device_id=None)
                 sys.exit() #added this
+
 
             if event.type == pygame.USEREVENT and started:
                     current_seconds -= 1
@@ -195,43 +266,46 @@ def main():
                     else:
                         started = True
 
+
+                #
+
                 #updating pomodoro settings
                 if TsettingRect.collidepoint(pos) :
                     popup = pygame.display.set_mode((300, 300))
                     popupDisplay = True
 
                     #buttons setup
-                    pUpButton = pygame.image.load('images/arrow.png').convert()
+                    pUpButton = pygame.image.load('images/arrow.png').convert_alpha()
                     pUpButton = pygame.transform.scale(pUpButton, (30, 30))
                     pUpButton = pygame.transform.rotate(pUpButton, 90)
                     pUpRect = pUpButton.get_rect()
                     pUpRect.center = (75, 75)
 
-                    pDownButton = pygame.image.load('images/arrow.png').convert()
+                    pDownButton = pygame.image.load('images/arrow.png').convert_alpha()
                     pDownButton = pygame.transform.scale(pDownButton, (30, 30))
                     pDownButton = pygame.transform.rotate(pDownButton, 270)
                     pDownRect = pDownButton.get_rect()
                     pDownRect.center = (225, 75)
 
-                    sbUpButton = pygame.image.load('images/arrow.png').convert()
+                    sbUpButton = pygame.image.load('images/arrow.png').convert_alpha()
                     sbUpButton = pygame.transform.scale(sbUpButton, (30, 30))
                     sbUpButton = pygame.transform.rotate(sbUpButton, 90)
                     sbUpRect = sbUpButton.get_rect()
                     sbUpRect.center = (75, 150)
 
-                    sbDownButton = pygame.image.load('images/arrow.png').convert()
+                    sbDownButton = pygame.image.load('images/arrow.png').convert_alpha()
                     sbDownButton = pygame.transform.scale(sbDownButton, (30, 30))
                     sbDownButton = pygame.transform.rotate(sbDownButton, 270)
                     sbDownRect = sbDownButton.get_rect()
                     sbDownRect.center = (225, 150)
 
-                    lbUpButton = pygame.image.load('images/arrow.png').convert()
+                    lbUpButton = pygame.image.load('images/arrow.png').convert_alpha()
                     lbUpButton = pygame.transform.scale(lbUpButton, (30, 30))
                     lbUpButton = pygame.transform.rotate(lbUpButton, 90)
                     lbUpRect = lbUpButton.get_rect()
                     lbUpRect.center = (75, 225)
 
-                    lbDownButton = pygame.image.load('images/arrow.png').convert()
+                    lbDownButton = pygame.image.load('images/arrow.png').convert_alpha()
                     lbDownButton = pygame.transform.scale(lbDownButton, (30, 30))
                     lbDownButton = pygame.transform.rotate(lbDownButton, 270)
                     lbDownRect = lbDownButton.get_rect()
@@ -310,7 +384,106 @@ def main():
                 elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
                     if(settings and bar_.get_rect().collidepoint(pygame.mouse.get_pos())): #adding for city
                         user_input += event.unicode
-                
+            
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box1.get_rect(center=(110, 163)).collidepoint(pygame.mouse.get_pos())): user_input1 = user_input1[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box1.get_rect(center=(110, 163)).collidepoint(pygame.mouse.get_pos())):
+                        user_input1 += event.unicode
+
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box2.get_rect(center=(125, 203)).collidepoint(pygame.mouse.get_pos())): user_input2 = user_input2[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box2.get_rect(center=(125, 203)).collidepoint(pygame.mouse.get_pos())):
+                        user_input2 += event.unicode
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box3.get_rect(center=(125, 243)).collidepoint(pygame.mouse.get_pos())): user_input3 = user_input3[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box3.get_rect(center=(125, 243)).collidepoint(pygame.mouse.get_pos())):
+                        user_input3 += event.unicode
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box4.get_rect(center=(125, 283)).collidepoint(pygame.mouse.get_pos())): user_input4 = user_input4[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box4.get_rect(center=(125, 283)).collidepoint(pygame.mouse.get_pos())):
+                        user_input4 += event.unicode
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_BACKSPACE):
+                    if(box5.get_rect(center=(125, 323)).collidepoint(pygame.mouse.get_pos())): user_input5 = user_input5[:-1]
+                elif (event.key >= pygame.K_a and event.key <= pygame.K_z) or pygame.K_COMMA:
+                    if(box5.get_rect(center=(125, 323)).collidepoint(pygame.mouse.get_pos())):
+                        user_input5 += event.unicode
+            pos = pygame.mouse.get_pos()
+            
+        if button1.collidepoint(pos) or button2.collidepoint(pos) or button3.collidepoint(pos) or button4.collidepoint(pos) or button5.collidepoint(pos):
+            if button1.collidepoint(pos):
+                screen.blit(check_mark, (38, 155))
+            if button2.collidepoint(pos):
+                screen.blit(check_mark, (38, 195))
+            if button3.collidepoint(pos):
+                screen.blit(check_mark, (38, 235))
+            if button4.collidepoint(pos):
+                screen.blit(check_mark, (38, 275))
+            if button5.collidepoint(pos):
+                screen.blit(check_mark, (38, 315))
+
+            # Disappearing and Reappearing for Checkmarks.
+            # As well as selecting and deselecting.
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = pygame.mouse.get_pos()
+                if button1.collidepoint(click) or button2.collidepoint(click) or button3.collidepoint(click) or button4.collidepoint(click) or button5.collidepoint(pos):
+                    image_click = True
+                    # Houses the Checkmark Functionality
+            
+            if image_click:
+                image_click = False
+                if button1.collidepoint(click):
+                    screen.blit(check_mark, (38, 155))
+                    button1_state = not button1_state
+                if button2.collidepoint(click):
+                    screen.blit(check_mark, (38, 195))
+                    button2_state = not button2_state
+                if button3.collidepoint(click):
+                    screen.blit(check_mark, (38, 235))
+                    button3_state = not button3_state
+                if button4.collidepoint(click):
+                    screen.blit(check_mark, (38, 275))
+                    button4_state = not button4_state
+                if button5.collidepoint(click):
+                    screen.blit(check_mark, (38, 315))
+                    button5_state = not button5_state
+
+        if button1_state:
+            screen.blit(check_mark, (38, 155))
+        if button2_state:
+            screen.blit(check_mark, (38, 195))
+        if button3_state:
+            screen.blit(check_mark, (38, 235))
+        if button4_state:
+            screen.blit(check_mark, (38, 275))
+        if button5_state:
+            screen.blit(check_mark, (38, 315))
+
+        user_input1 = user_input1[:14]
+        user_input2 = user_input2[:14]
+        user_input3 = user_input3[:14]
+        user_input4 = user_input4[:14]
+        user_input5 = user_input5[:14]
+        AddText(screen, 20, user_input1, (0,0,0), 70, 160)
+        AddText(screen, 20, user_input5, (0,0,0), 70, 320)
+        AddText(screen, 20, user_input3, (0,0,0), 70, 240)
+        AddText(screen, 20, user_input4, (0,0,0), 70, 280)
+        AddText(screen, 20, user_input2, (0,0,0), 70, 200)
+        screen.blit(background_4_timer, (260,148))
+        screen.blit(spotify_bar, (0,380))
         if spotifyWorking:
             #displaying lines
             pygame.draw.line(screen, (157, 163, 245), 
@@ -393,6 +566,7 @@ def main():
         timer_text = Tfont.render(f"{display_minutes:02}:{display_seconds:02}", True, "white")
         timer_text_rect = timer_text.get_rect()
         timer_text_rect.center = (325, 200)
+        
         screen.blit(timer_text, timer_text_rect)
         screen.blit(TsettingButton, TsettingRect)
         if round % 6 == 0:
@@ -432,24 +606,18 @@ def main():
                     add_12 = timedelta(hours=12)
                     local_time = local_time + add_12
                     sunrise_time = sunrise_time + add_12
-                    
-                    
-            
-
 
             weather_picture = (weather_icons.get(weather_condition, {}).get(day_or_night))
             weather = pygame.image.load(weather_picture).convert_alpha()
             bar_path = bar.get(day_or_night)
             bar_ = pygame.image.load(bar_path).convert_alpha()
             settings_bar = pygame.image.load(bar_path).convert_alpha()
-            
-
+        
             if(day_or_night == "day"):
                 weather_color = (0,0,0)
             else:
                 weather_color = (255,255,255)
 
-            
             if(not military_time):
                 if(00 <= local_time_for_non_military.hour < 12):
                     time_thing = "AM"
@@ -464,15 +632,11 @@ def main():
                 sun = sunrise_time.strftime("%H:%M")
                 sun_pic = pygame.image.load('images/sunrise.png').convert_alpha()
 
-
-
             #Images
             screen.blit(bar_,(25,10))
             screen.blit(weather, (30,20))
             screen.blit(sun_pic, (135, 85))
             
-
-
             # Text
             AddText(screen, 20, sun, weather_color, 145, 87)
             if(not military_time): AddText(screen, 18, time_thing, weather_color, 360, 50)
@@ -494,8 +658,7 @@ def main():
                 settings = False
                 if(user_input!= ""):city = user_input
                 else: city = city
-        
-            
+          
             if(unit == "F"): coordinates_for_paw = (30, 42)
             elif(unit == "C"): coordinates_for_paw = (30, 60)
             elif(unit == "K"): coordinates_for_paw = (30, 78)
@@ -505,9 +668,6 @@ def main():
             kelvin_button = Button(30, 78, empty_icon)
 
             screen.blit(empty_icon, (30, 21))
-            
-            
-
 
             if far_button.draw(screen) and settings:
                 unit = "F"
@@ -521,9 +681,6 @@ def main():
                     military_time = False
                 else:
                     military_time = True
-
-            
-            
             
             screen.blit(empty_icon, (30, 42))
             AddText(screen, 18, "Fahrenheit", weather_color, 50, 42)
@@ -538,11 +695,10 @@ def main():
             screen.blit(insert_city_bar, (140, 78))
             AddText(screen, 20, user_input,(0,0,0), 140, 78)
             AddText(screen, 20, "Change City :P", weather_color, 140, 55)
-            
-
 
         pygame.display.update()
 
     pygame.quit()
 
-main()
+if __name__ == "__main__":
+    main()
